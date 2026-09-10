259a24c61245bace95d740798ed066e2c9238759
BerniUCB <bernardo.rios@ucb.edu.bo>
2026-09-09 20:09:45 -0400

    feat: mostrar detalle completo del calculo en UI

--- src/presenter.js
+++ src/presenter.js
@@ -26,11 +26,11 @@ form.addEventListener("submit", (event) => {
   }
 
   div.innerHTML = `
-    <p>Precio neto (${resultado.cantidad}*$${resultado.precio}): $${resultado.precioNeto}</p>
-    <p>Descuento (${resultado.porcentajeDescuento}%): $${resultado.descuento.toFixed(2)}</p>
-    <p>Impuesto para ${resultado.estado} (%${resultado.porcentajeImpuesto}): $${resultado.impuesto.toFixed(2)}</p>
-    <p>Costo de envio: $${resultado.costoEnvio.toFixed(2)}</p>
+    <p>Precio neto: $${resultado.precioNeto}</p>
+    <p>Impuesto (estado ${resultado.estado} ${resultado.porcentajeImpuestoEstado}% + cat. ${resultado.porcentajeImpuestoCategoria}%): $${resultado.impuesto.toFixed(2)}</p>
+    <p>Descuento (${resultado.porcentajeDescuentoNeto}% + ${resultado.porcentajeDescuentoCategoria}% cat.): $${resultado.descuento.toFixed(2)}</p>
     <p>Descuento fijo: $${resultado.descuentoFijo.toFixed(2)}</p>
-    <p>Precio total (descuento e impuesto): $${resultado.precioTotal.toFixed(2)}</p>
+    <p>Costo de envio: $${resultado.costoEnvio.toFixed(2)}</p>
+    <p>Precio total: $${resultado.precioTotal.toFixed(2)}</p>
   `;
 });
